92744ee92f6b541335f9c2ef9f85a6fa4db7cdff
davidsolarte900 <davidsolarte900@gmail.com>
2026-05-03 12:38:39 -0500

    trabaje en la investigacion

---
+++ js/index.js
@@ -0,0 +1,18 @@
+/*const saludar = () => {
+    console.log(`hola!!!!!!!!!!!!!!!!`);
+};
+
+saludar()*/
+
+const edad = 25;
+const experiencia = true;
+if (edad >= 18) {
+    console.log(`puedes aplicar apuesto senior.`);
+}
+{
+    console.log(`puedes aplicar a puesto junior.`);
+}
+{
+    console.log(`aun no puedes aplicar.`);
+}
+
