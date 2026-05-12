fe01f31fe9bfcbb4a9365f12ebe1b383e9da7853
davidsolarte900 <davidsolarte900@gmail.com>
2026-05-11 21:28:33 -0500

    trabajae en javascript

---
+++ js/inves.js
@@ -0,0 +1,22 @@
+//console.log(`hola david`)
+let miVariable = 24;
+//console.log(`mi edad es :${miVariable}`);
+
+miVariable = 12;
+//console.log(`mi edad es : ${miVariable}`)
+let op1 = 2
+let op2 = 3
+let resultado = op1 + op2;
+console.log(`el resultado es ${resultado}`)
+
+// condicionales
+
+let miNumero = 6
+//let resultadoPregunta = miNumero == 6;
+//console.log(resultadoPregunta);
+
+
+if(miNumero == 7){
+    console.log(`si. mi numero vale 6`);
+} else {  console.log(`No. mi numero no vale 6`);
+}
