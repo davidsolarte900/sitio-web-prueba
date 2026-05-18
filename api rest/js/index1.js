0a1b3943149bd7c6c6cb930d71c00b4eb12e9efe
davidsolarte900 <davidsolarte900@gmail.com>
2026-05-18 17:59:24 -0500

    consumir api rick and morty

--- api rest/js/index1.js
+++ api rest/js/index1.js
@@ -1,13 +1,22 @@
 const contenedor = document.getElementById(`contenedor`);
 
-const obetenerPersonajes =  async() => {
+const obtenerPersonajes = async() => {
     try {
+        // funcion fetch trae una promesa y la eseramos con await
         const respuesta = await fetch(`https://rickandmortyapi.com/api/character`);
-        console.log(respuesta)
+        const data = await respuesta.json();
+        console.log(data.results);
+        data.results.forEach((personaje) => {
+            //console.log(personaje.name);
+            contenedor.innerHTML += `
+            <div class="col-md-4 col-lg-3">
+                <h2>${personaje.name}</h2>
+            </div>
+            `;
+        });
     } catch(error) {
-        console.log(` este es un error atrapado por el catch : ${error}`);
+        console.log(`este es un error atrapado por el catch: ${ error }`);
     }
 };
 
-obetenerPersonajes();
-
+obtenerPersonajes ();
