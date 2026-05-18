817a5c5ed8e3321c21389fb6549c059dd4468e90
davidsolarte900 <davidsolarte900@gmail.com>
2026-05-18 12:11:56 -0500

    arreglo de carpetas

---
+++ prueba.js
@@ -0,0 +1,20 @@
+// investigacion 3
+//
+
+
+
+// const http = require('node:http');
+
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hola Mundo desde Node.js!');
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Servidor ejecutándose en http://${hostname}:${port}/`);
+// });
+
