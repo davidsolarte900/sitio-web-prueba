42d309c7f2d16f7b8bcfdae89a4bc4e2fa62817e
davidsolarte900 <davidsolarte900@gmail.com>
2026-06-28 10:47:43 -0500

    trabaje en registro y login

--- backend/validator/auth.validator.js
+++ backend/validator/auth.validator.js
@@ -1,17 +1,30 @@
 const { body } = require('express-validator');
+const { notify } = require("../routes/auth.routes");
 
 const registerValidator = [
     body('nombre')
         .notEmpty().withMessage('El nombre es obligatorio')
-        .isLength({}).withMessage('minimo 3 caracteres'),
+        .isLength({ min: 4 }).withMessage('minimo 4 caracteres'),
     body('email')
         .notEmpty().withMessage('El email es obligatorio')
         .isEmail().withMessage('Debes enviar un email valido'),
     body('password')
         .notEmpty().withMessage('La contraseña es obligatorio')
-        .isLength({ min: 6 }).withMessage('La constraseña debe tener minimo 8 caracteres mayus, minus, numeros y caracteres especiales')    
+        .isLength({ min: 6 }).withMessage('La constraseña debe tener minimo 6 caracteres entre numeros y letras')    
+];
 
-]
+const loginValidator = [
+    body('email')
+        .notEmpty().withMessage('El email es obligatorio')
+        .isEmail().withMessage('Debes ingresar un email valido').normalizeEmail(),
+    body('password')
+        .notEmpty().withMessage('La contraseña es obligatoria') 
+];
+
+
+
+
 module.exports = {
-    registerValidator
+    registerValidator,
+    loginValidator    
 }
