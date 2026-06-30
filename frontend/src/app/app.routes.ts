7a34262bf344c477e32d4ed2465200d283158eec
davidsolarte900 <davidsolarte900@gmail.com>
2026-06-30 18:45:19 -0500

    trabaje en instalaciones

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { TaskComponent } from './pages/tasks/task.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    },    
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'register',
+        component: RegisterComponent
+    },
+    {
+        path: 'tasks',
+        component: TaskComponent
+    }
+];
+
