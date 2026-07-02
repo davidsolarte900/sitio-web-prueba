a92d1140fb63957dd09525acf459a1771554ad9a
davidsolarte900 <davidsolarte900@gmail.com>
2026-07-01 20:56:00 -0500

    trabaje en parte de organizar frontend

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,5 +1,7 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { importProvidersFrom } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
@@ -9,6 +11,6 @@ export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
-    // provideAnimations()
+    importProvidersFrom(ReactiveFormsModule)
   ]
 };
